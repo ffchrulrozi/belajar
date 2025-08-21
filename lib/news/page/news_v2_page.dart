650bddfd6794fd0a0f2f7b8aeb103e7056411398
ffchrulrozi <ffchrulrozi@gmail.com>
2025-08-21 14:51:39 +0900

    give comment to v2 codes

--- lib/news/page/news_v2_page.dart
+++ lib/news/page/news_v2_page.dart
@@ -12,41 +12,56 @@ class NewsV2Page extends StatefulWidget {
 }
 
 class _NewsV2PageState extends State<NewsV2Page> {
+  // dio adalah library buat integrasi api
   final dio = Dio();
   var isLoading = false;
+  // everything adalah sebuah class model (format/struktur response api)
   var apiResponse = Everything();
   var search = "yogyakarta";
   var pageSize = 5;
 
+  // initState adalah sebuah function yang pertama kali dipanggil
   @override
   void initState() {
     super.initState();
+    // panggil function getApi() ketika baru init app
     getApi();
   }
 
   getApi() async {
+    // pada saat get api, untuk awalnya set loading tre
     setState(() {
       isLoading = true;
     });
 
     if (search == "") {
       setState(() {
+        // jika search kosong, maka set data kosong
         apiResponse = Everything();
         isLoading = false;
       });
     } else {
       final url =
           "https://newsapi.org/v2/everything?language=id&from=2025-08-01&sortBy=publishedAt&apiKey=c807a263fd1443a99d906c8959b602d9&q=$search&pagesize=$pageSize";
+
+      // dio.get adalah proses get data dari api
       final response = await dio.get(url);
+
+      // statusCode=200 berarti data berhasil diambil, sedangkan yang lain kemungkinan error
+      // misal: 400=error body, 401=gagal autentikasi, 404=data tidak ada, 500=error server
       if (response.statusCode == 200) {
+        // jika statusCode=200, maka encode kan data api tersebut
         var encodedData = jsonEncode(response.data);
+        // lalu di parsing ke dalam class model dengan memanggil function everythingFromJson
         var parsedResponse = everythingFromJson(encodedData);
 
+        // setelah sukses, maka set state apiResponse dan loading=false
         setState(() {
           apiResponse = parsedResponse;
           isLoading = false;
         });
       } else {
+        // tampilkan errornya apa di console
         debugPrint("API error: ${response.statusMessage}");
       }
     }
@@ -89,11 +104,14 @@ class _NewsV2PageState extends State<NewsV2Page> {
         ],
       ),
       body: isLoading
+          // jika loading=true, maka show icon loading
           ? Center(child: CircularProgressIndicator())
+          // jika artikel kosong, maka tampilkan pesan "tidak ada data"
           : apiResponse.articles == null || apiResponse.articles?.length == 0
               ? Center(
                   child: Text("Tidak ada data"),
                 )
+              // jika datanya ada
               : ListView.builder(
                   itemCount: apiResponse.articles!.length,
                   itemBuilder: (context, index) {
